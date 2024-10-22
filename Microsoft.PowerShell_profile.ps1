061b1291249ea643d02da8febb303e35a1b16e8f
Jeff Patterson <jpatterson@ncswash.com>
2024-10-22 08:00:58 -0500

    Added comments to the profile file.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,7 @@
 oh-my-posh init pwsh | Invoke-Expression
-Set-Alias -Name repos $env:USERPROFILE\Documents\PowerShell\Get-Repo.ps1 -Option AllScope
-Set-Alias -Name tunnel $env:USERPROFILE\Documents\PowerShell\Get-Tunnel.ps1 -Option AllScope
-Set-Alias -Name startup $env:USERPROFILE\Documents\PowerShell\Get-Startup.ps1 -Option AllScope
+
+#Aliases are not case sensitive in Windows.
+Set-Alias -Name Repos $env:USERPROFILE\Documents\PowerShell\Get-Repo.ps1 -Option AllScope
+Set-Alias -Name Tunnel $env:USERPROFILE\Documents\PowerShell\Get-Tunnel.ps1 -Option AllScope
+Set-Alias -Name Startup $env:USERPROFILE\Documents\PowerShell\Get-Startup.ps1 -Option AllScope
 Set-Alias -Name PSRepo $env:USERPROFILE\Documents\PowerShell\Get-PSRepo.ps1 -Option AllScope
