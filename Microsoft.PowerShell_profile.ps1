b9f94a0947f040d614f4e63a66049989c1f8c2b9
Jeff Patterson <jpatterson@ncswash.com>
2024-10-23 13:16:25 +0000

    Update Microsoft.PowerShell_profile.ps1 to comment out an optional extension. It is likely that the other users might have a customized one that they wouldn't want overwritten. If you use it, create a branch with your name to differentiate it from the original "template," and don't be afraid to merge occasionally if you see something you like. And please share if you have something fun or useful to add.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,7 @@
-oh-my-posh init pwsh | Invoke-Expression
+# This line is used for the CLI Extension oh-my-posh
+# that adds useful info and color to the prompt.
+# oh-my-posh init pwsh | Invoke-Expression
+
 
 #Aliases are not case sensitive in Windows.
 Set-Alias -Name Repos $env:USERPROFILE\Documents\PowerShell\Get-Repo.ps1 -Option AllScope
