dbb5e18cdf875f5d8ce35523eb3436200d3baadd
Jeff Patterson <jpatterson@ncswash.com>
2024-12-03 16:59:30 +0000

    Cleaned up the output.

--- Get-RepoStatus.ps1
+++ Get-RepoStatus.ps1
@@ -1,13 +1,68 @@
+<#
+.SYNOPSIS
+    This script checks for changes in all repositories from Azure DevOps and GitLab.
+.DESCRIPTION
+    The script will output the number of repositories in each system and the number
+    of changes in each repository along with the SHA, committer, date, and message.
+.PARAMETER h -OR help
+    Display the help message.
+.PARAMETER verbose -or v
+    Display verbose output.
+.EXAMPLE
+    PS> .\Get-RepoStatus.ps1 -verbose
+.EXAMPLE
+    PS> .\Get-RepoStatus.ps1 -v
+.EXAMPLE
+    PS> .\Get-RepoStatus.ps1 -help
+.EXAMPLE
+    PS> .\Get-RepoStatus.ps1 -h
+
+.NOTES
+    File Name      : Get-RepoStatus.ps1
+    Author         : Jeff Patterson
+    Prerequisite   : PowerShell V7
+    Date           : 2024-11-27
+#>
+
+param (
+    [switch]$help,
+    [switch]$h,
+    [switch]$verbose,
+    [switch]$v
+)
+
+if($h -or $help) {
+    Get-Help -Full $MyInvocation.MyCommand.Path
+    exit
+}
+
+if($v -or $verbose) {
+    $showall = $true
+    Write-Host "Verbose output is enabled."
+}
+
+Clear-Host
 Write-Host "--------------------------------------------------------------------------------"
 Write-Host "Checking the Azure DevOps Repositories using the Alias 'AzD'"
 Write-Host "--------------------------------------------------------------------------------"
-AzD
+if ($showall) {
+    AzD -verbose
+} else {
+    AzD
+}
 
 Write-Host "--------------------------------------------------------------------------------"
 Write-Host "Checking the GitLab Repositories using the Alias 'gitlab'"
 Write-Host "--------------------------------------------------------------------------------"
-gitlab
+if($showall) {
+    gitlab -verbose
+} else {
+    gitlab
+}
 
 Write-Host "--------------------------------------------------------------------------------"
-Write-Host "Checking Completed"
+Write-Host "Checking is complete."
 Write-Host "--------------------------------------------------------------------------------"
+Write-Host ""
+Write-Host ""
+
--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,15 @@
-# This line is used for the CLI Extension oh-my-posh
-# that adds useful info and color to the prompt.
-# oh-my-posh init pwsh | Invoke-Expression
+# Snippet to test for the current user
+if ($env:USERNAME -eq "JPatterson") { }
 
+# Test if an extension is installed and if not install it.
+if (Get-Command oh-my-posh -ErrorAction SilentlyContinue) {
+    # This line is used for the CLI Extension oh-my-posh
+    # that adds useful info and color to the prompt.
+    oh-my-posh init pwsh | Invoke-Expression
+}
+else {
+    Write-Host "oh-my-posh not initialized."
+}
 
 # Simplify the path to the PowerSehll scripts and make the lines shorter.
 $ScriptLocation = Join-Path $env:USERPROFILE "\Documents\PowerShell\"
@@ -28,3 +36,34 @@ Set-Alias -Name repostatus $ScriptLocation\Get-RepoStatus.ps1 -Option AllScope
 # Command Aliases to make it easier to enter CLI commands
 # List all files in the current directory including hidden
 Set-Alias -Name lsf $ScriptLocation\Get-ChildItem-Force.ps1 -Option AllScope 
+
+if($env:USERNAME -eq "JPatterson") {
+    Set-Alias -Name webdev $ScriptLocation\Get-Folder-WebDev.ps1 -Option AllScope
+
+    # Git Shortcut Aliases
+    function Get-GitStatus { & git status $args}
+    Set-Alias -Name status Get-GitStatus -Option AllScope
+
+    function Get-GitFetchAll { & git fetch --all}
+    Set-Alias -name fetch Get-GitFetchAll -Option AllScope
+
+    function Get-GitAddAll { & git add .}
+    Set-Alias -Name addall Get-GitAddAll -Option AllScope
+
+    function Get-GitPull { & git pull $args}
+    Set-Alias -Name pull Get-GitPull -Option AllScope
+
+    function Get-GitPush { & git push $args}
+    Set-Alias -Name push Get-GitPush -Option AllScope
+
+    function Get-GitCommit { & git commit $args}
+    Set-Alias -name commit Get-GitCommit -Option AllScope
+
+    function Get-GitSwitch { & git switch $args}
+    Set-Alias -name gswitch Get-GitSwitch -Option AllScope
+}
+
+else {
+    Write-Host "The user $env:USERNAME does not have that alias."
+}
+
